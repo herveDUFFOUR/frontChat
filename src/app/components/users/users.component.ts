d873e92bc2dc4566a2df4a76dbeb93b927a01735
herve DUFFOUR <herve.duffour@gmail.com>
2022-03-30 19:23:03 +0200

    show users

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -2,6 +2,7 @@ import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-users',
@@ -12,6 +13,9 @@ export class UsersComponent implements OnInit {
 
   userSub!: Subscription;
 
+  users: User[] = []
+
+
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
@@ -19,8 +23,17 @@ export class UsersComponent implements OnInit {
         this.userSub = this.userService.findAll().subscribe((response: HttpResponse<any>) => {
 
           if(response.status == 200){
-            console.log(response)
-            console.log(response.body)
+
+              const responseArray = JSON.parse(response.body)
+
+              responseArray.forEach((u: any) => {
+
+                let user: User = {login: u.login,password: u.password,name: u.name}
+
+                this.users.push(user)
+
+              });
+
           }
         })
   }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,4 +1,5 @@
 export interface User {
   login: string,
-  password: string
+  password: string,
+  name?: string
 }
